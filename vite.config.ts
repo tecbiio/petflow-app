b065e95e8a6f7b7df38f9eb1d49ef414463f1829
tecbiio <lorenzelli.antony@gmail.com>
2026-03-03 21:31:54 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    strictPort: true,
+    host: true,
     port: 5173,
+    allowedHosts: ["www.mypetflow.fr", "mypetflow.fr"],
   },
   preview: {
+    strictPort: true,
     host: true,
     port: 5173,
     allowedHosts: ["www.mypetflow.fr", "mypetflow.fr"],
